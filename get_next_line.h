4c193c3647312bb15ac0e31329f6c96677ad1c77
VanAntonietti <v.antonietti@icloud.com>
2021-11-15 23:16:39 -0300

    1.0 finished

---
+++ get_next_line.h
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vantonie <vantonie@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/26 20:29:29 by vantonie          #+#    #+#             */
+/*   Updated: 2021/10/04 23:39:18 by vantonie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef FD_SIZE
+#  define FD_SIZE 256
+# endif
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+/**
+ * @brief Function to get a next file from a fd.
+ *
+ * @param fd to be search
+ * @return the address of the first N.
+ */
+/**
+ * @brief Function to get a next file from a fd.
+ *
+ * @param fd to be search
+ * @return the address of the first N.
+ */
+char	*get_next_line(int fd);
+
+/**
+ * @brief Function locates the first N in the STR.
+ *
+ * @param str to be search
+ * @param n to be find
+ * @return the address of the first N.
+ */
+char	*ft_strchr(const char *str, int n);
+
+/**
+ * @brief 	Function returns the length of STR.
+ *
+ * @param str string to be counted.
+ * @return length of the STR.
+ */
+size_t	ft_strlen(const char *str);
+
+/**
+ * @brief Copy the STR to a new pointer.
+ *
+ * @param str the string to be copied.
+ * @return a new address with the same string of str.
+ */
+char	*ft_strdup(const char *str);
+
+/**
+ * @brief Copy the from the (S + START) to (LEN).
+ *
+ * @param s the address of the start of the string
+ * @param start start the copy from here.
+ * @param len number of the bytes to be copied.
+ * @return a new address to with the copy.
+ */
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+/**
+ * @brief Copy the S1 concatenate with S2.
+ *
+ * @param s1 string to start the new string.
+ * @param s2 string to end the new string.
+ * @return new address with the s1 + s2.
+ */
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
---
+++ get_next_line_bonus.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vantonie <vantonie@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/21 21:37:10 by vantonie          #+#    #+#             */
+/*   Updated: 2021/10/04 23:42:02 by vantonie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*read_sort(int new_line_position, char **tmp)
+{
+	char	*res;
+	char	*hold;
+
+	if (new_line_position <= 0)
+	{
+		if (**tmp == '\0')
+		{
+			free (*tmp);
+			*tmp = NULL;
+			return (NULL);
+		}
+		res = *tmp;
+		*tmp = NULL;
+		return (res);
+	}
+	hold = ft_substr(*tmp, new_line_position, BUFFER_SIZE);
+	res = *tmp;
+	res[new_line_position] = 0;
+	*tmp = hold;
+	return (res);
+}
+
+char	*read_content(int fd, char *content, char **tmp)
+{
+	ssize_t	num_bytes;
+	char	*new_line;
+	char	*hold;
+
+	new_line = ft_strchr(*tmp, '\n');
+	while (new_line == NULL)
+	{
+		num_bytes = read(fd, content, BUFFER_SIZE);
+		if (num_bytes <= 0)
+		{
+			return (read_sort(num_bytes, tmp));
+		}
+		content[num_bytes] = '\0';
+		hold = ft_strjoin(*tmp, content);
+		free(*tmp);
+		*tmp = hold;
+		new_line = ft_strchr(*tmp, '\n');
+	}
+	return (read_sort(new_line - *tmp + 1, tmp));
+}
+
+char	*get_next_line(int fd)
+{
+	char		*read_content_return;
+	char		*reservoir;
+	static char	*tmp[FD_SIZE + 1];
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > FD_SIZE)
+	{
+		return (NULL);
+	}
+	reservoir = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (reservoir == NULL)
+	{
+		return (NULL);
+	}
+	if (tmp[fd] == NULL)
+	{
+		tmp[fd] = ft_strdup("");
+	}
+	read_content_return = read_content(fd, reservoir, &tmp [fd]);
+	if (reservoir != NULL)
+	{
+		free (reservoir);
+		reservoir = NULL;
+	}
+	return (read_content_return);
+}
